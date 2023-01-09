ef2f0d2b02de1ce5740acfc8c536c65e736d9f1e
Dee Kay <dk@email.com>
2023-01-09 23:53:40 +0000

    change between cross and naught

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return <button className="square" onClick={() => this.props.omClick()}>{this.props.value}</button>;
-  }
+function Square(props) {
+  return(
+    <button className="square" onClick={props.onClick}>{props.value}</button>
+  );
 }
 
 class Board extends React.Component {
@@ -13,7 +13,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -21,7 +31,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
